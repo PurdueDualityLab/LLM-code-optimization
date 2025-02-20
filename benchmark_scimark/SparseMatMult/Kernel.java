87a534f0ee103db3c5b9ee551063c54f27a11fa0
Leo Deng <leo7deng@gmail.com>
2025-02-20 10:14:27 -0500

    Create seperate directories for each scimark benchmark

--- benchmark_scimark/SparseMatMult/Kernel.java
+++ benchmark_scimark/SparseMatMult/Kernel.java
@@ -3,71 +3,6 @@ package jnt.scimark2;
 public class Kernel {
     // each measurement returns approx Mflops
 
-
-    public static double measureFFT(int N, double mintime, Random R) {
-        // initialize FFT data as complex (N real/img pairs)
-
-        double[] x = RandomVector(2 * N, R);
-        double[] oldx = NewVectorCopy(x);
-        long cycles = 1;
-        Stopwatch Q = new Stopwatch();
-
-        while (true) {
-            Q.start();
-            for (int i = 0; i < cycles; i++) {
-                FFT.transform(x);    // forward transform
-                FFT.inverse(x);        // backward transform
-            }
-            Q.stop();
-            if (Q.read() >= mintime)
-                break;
-
-            cycles *= 2;
-        }
-        // approx Mflops
-
-        final double EPS = 1.0e-10;
-        if (FFT.test(x) / N > EPS)
-            return 0.0;
-
-        return FFT.num_flops(N) * cycles / Q.read() * 1.0e-6;
-    }
-
-
-    public static double measureSOR(int N, double min_time, Random R) {
-        double[][] G = RandomMatrix(N, N, R);
-
-        Stopwatch Q = new Stopwatch();
-        long cycles = 1;
-        while (true) {
-            Q.start();
-            SOR.execute(1.25, G, cycles);
-            Q.stop();
-            if (Q.read() >= min_time) break;
-
-            cycles *= 2;
-        }
-        // approx Mflops
-        return SOR.num_flops(N, N, cycles) / Q.read() * 1.0e-6;
-    }
-
-    public static double measureMonteCarlo(double min_time, Random R) {
-        Stopwatch Q = new Stopwatch();
-
-        long cycles = 1;
-        while (true) {
-            Q.start();
-            MonteCarlo.integrate(cycles);
-            Q.stop();
-            if (Q.read() >= min_time) break;
-
-            cycles *= 2;
-        }
-        // approx Mflops
-        return MonteCarlo.num_flops(cycles) / Q.read() * 1.0e-6;
-    }
-
-
     public static double measureSparseMatmult(int N, int nz,
                                               double min_time, Random R) {
         // initialize vector multipliers and storage for result
@@ -134,47 +69,6 @@ public class Kernel {
         // approx Mflops
         return SparseCompRow.num_flops(N, nz, cycles) / Q.read() * 1.0e-6;
     }
-
-
-    public static double measureLU(int N, double min_time, Random R) {
-        // compute approx Mlfops, or O if LU yields large errors
-
-        double[][] A = RandomMatrix(N, N, R);
-        double[][] lu = new double[N][N];
-        int[] pivot = new int[N];
-
-        Stopwatch Q = new Stopwatch();
-
-        long cycles = 1;
-        while (true) {
-            Q.start();
-            for (int i = 0; i < cycles; i++) {
-                CopyMatrix(lu, A);
-                LU.factor(lu, pivot);
-            }
-            Q.stop();
-            if (Q.read() >= min_time) break;
-
-            cycles *= 2;
-        }
-
-
-        // verify that LU is correct
-        double[] b = RandomVector(N, R);
-        double[] x = NewVectorCopy(b);
-
-        LU.solve(lu, pivot, x);
-
-        final double EPS = 1.0e-12;
-        if (normabs(b, matvec(A, x)) / N > EPS)
-            return 0.0;
-
-
-        // else return approx Mflops
-        //
-        return LU.num_flops(N) * cycles / Q.read() * 1.0e-6;
-    }
-
 
     private static double[] NewVectorCopy(double[] x) {
         int N = x.length;
